df39e7f7a8a1d9e2d8230468409c996250923b71
Manikgog <m4xin4t4@gmail.com>
2023-04-04 14:45:26 +0500

    Addition 1 raw

--- main.cpp
+++ main.cpp
@@ -19,58 +19,58 @@ void print_arr2D(T**& arr, int raws, int cols);
 
 
 int main(){
-	setlocale(LC_ALL, "Russian");
-	srand(time(NULL));
-	int raw = 4, col = 5;
-	int** arr = new int*[raw];
-	for(int i = 0; i < raw; i++){
-		arr[i] = new int[col];
-		for(int j = 0; j < col; j++){
-			arr[i][j] = rand()%10;
-		}
-	}
+        setlocale(LC_ALL, "Russian");
+        srand(time(NULL));
+        int raw = 5, col = 5;
+        int** arr = new int*[raw];
+        for(int i = 0; i < raw; i++){
+                arr[i] = new int[col];
+                for(int j = 0; j < col; j++){
+                        arr[i][j] = rand()%10;
+                }
+        }
 
-	print_arr2D(arr, raw, col);
+        print_arr2D(arr, raw, col);
 
-	bool flag = false;
-	for(int i = 0; i < raw; ){
-		flag = false;
-		for(int j = 0; j < col; j++){
-			if(arr[i][j] <= 0){
-				delete_row(arr, raw, col, i);
-				flag = false;
-				raw--;
-				break;
-			}else
-				flag = true;
-		}
-		if(flag)
-			i++;
-	}
-
-
-	std::cout << std::endl;
-	print_arr2D(arr, raw, col);
+        bool flag = false;
+        for(int i = 0; i < raw; ){
+                flag = false;
+                for(int j = 0; j < col; j++){
+                        if(arr[i][j] <= 0){
+                                delete_row(arr, raw, col, i);
+                                flag = false;
+                                raw--;
+                                break;
+                        }else
+                                flag = true;
+                }
+                if(flag)
+                        i++;
+        }
 
 
-	for(int i = 0; i < raw; i++){
-		delete[] arr[i];
-	}
-
-	delete[] arr;
+        std::cout << std::endl;
+        print_arr2D(arr, raw, col);
 
 
-	return 0;
+        for(int i = 0; i < raw; i++){
+                delete[] arr[i];
+        }
+
+        delete[] arr;
+
+
+        return 0;
 }
 
 template <typename T>
 void print_arr2D(T**& arr, int raws, int cols){
-	for(int i = 0; i < raws; i++){
-		for(int j = 0; j < cols; j++){
-			std::cout << arr[i][j] << '\t';
-		}
-		std::cout << std::endl;
-	}
+        for(int i = 0; i < raws; i++){
+                for(int j = 0; j < cols; j++){
+                        std::cout << arr[i][j] << '\t';
+                }
+                std::cout << std::endl;
+        }
 }
 
 
@@ -78,34 +78,31 @@ void print_arr2D(T**& arr, int raws, int cols){
 
 template <typename T>
 void delete_row(T**& arr, int raws, int cols, int raw_to_delete){
-	T** temp_arr = new T*[raws - 1];
-	for(int i = 0; i < raws - 1; i++){
-		temp_arr[i] = new T[cols];
-	}
+        T** temp_arr = new T*[raws - 1];
+        for(int i = 0; i < raws - 1; i++){
+                temp_arr[i] = new T[cols];
+        }
 
-	for (int i = 0; i < raw_to_delete; i++) {
-		for (int j = 0; j < cols; j++){
-			temp_arr[i][j] = arr[i][j];
-		}
-	}
+        for (int i = 0; i < raw_to_delete; i++) {
+                for (int j = 0; j < cols; j++){
+                        temp_arr[i][j] = arr[i][j];
+                }
+        }
 
-	for (int i = raw_to_delete + 1; i < raws; i++) {
-		for (int j = 0; j < cols; j++){
-			temp_arr[i - 1][j] = arr[i][j];
-		}
-	}
+        for (int i = raw_to_delete + 1; i < raws; i++) {
+                for (int j = 0; j < cols; j++){
+                        temp_arr[i - 1][j] = arr[i][j];
+                }
+        }
 
-	for(int i = 0; i < raws; i++){
-		delete[] arr[i];
-	}
+        for(int i = 0; i < raws; i++){
+                delete[] arr[i];
+        }
 
-	delete[] arr;
+        delete[] arr;
 
-	arr = temp_arr;
+        arr = temp_arr;
 
 }
-
-
-
 
 
